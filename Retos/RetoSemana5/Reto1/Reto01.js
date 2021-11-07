85c6ae03cf757e9218fb341316b07c01f9097a24
Alexander Blivar <37673464+uertex@users.noreply.github.com>
2021-11-07 18:05:59 -0500

    Update Reto01.js

--- Retos/RetoSemana5/Reto1/Reto01.js
+++ Retos/RetoSemana5/Reto1/Reto01.js
@@ -1,19 +1,27 @@
-alert(`Bievenido`);
+// alert(`Bievenido`);
 
-const conv = 3.785;
+// const conv = 3.785;
 
-let PrecioGalon = prompt("¿Cual es el precio del galon hoy?");
-let CantidadLitros = prompt("¿Cuantos litros produjo hoy?");
-let PGalon = parseFloat(PrecioGalon);
-let litros = parseInt(CantidadLitros);
-let conversion = conv*litros;
-let ganancia = conversion*PGalon;
+// let PrecioGalon = prompt("¿Cual es el precio del galon hoy?");
+// let CantidadLitros = prompt("¿Cuantos litros produjo hoy?");
+// let PGalon = parseFloat(PrecioGalon);
+// let litros = parseInt(CantidadLitros);
+// let conversion = conv*litros;
+// let ganancia = conversion*PGalon;
 
-console.log("Tipos de datos: ");
-console.log("tipo de dato de precio de galon: "+ typeof PGalon);
-console.log("tipo de dato de cantidad en litros: "+ typeof litros);
-console.log("tipo de dato de cantidad en galon: "+ typeof conv);
-console.log("tipo de dato de ganancia : "+ typeof ganancia);
+// console.log("Tipos de datos: ");
+// console.log("tipo de dato de precio de galon: "+ typeof PGalon);
+// console.log("tipo de dato de cantidad en litros: "+ typeof litros);
+// console.log("tipo de dato de cantidad en galon: "+ typeof conv);
+// console.log("tipo de dato de ganancia : "+ typeof ganancia);
 
-console.log(`Usted tiene en galones: ${conversion} galones`);
-console.log(`La ganancia del dia con el precio del galon(${PGalon}) es de ${ganancia} nuevos soles`);
+// console.log(`Usted tiene en galones: ${conversion} galones`);
+// console.log(`La ganancia del dia con el precio del galon(${PGalon}) es de ${ganancia} nuevos soles`);
+let energia = [22,25,28,30];
+// energia.forEach(function(lal){
+//     console.log(lal/2)
+// })
+let energy = energia.map(function(ener){
+    return ener/2;
+});
+console.table(energy)
